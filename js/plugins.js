357e39be756879f0907ef9020354e254c5df51d7
Meycko Jade Anthony Canoy <meyckocanoy@gmail.com>
2026-01-23 07:48:46 +0800

    Myportfolio Update
    - Make tabs in My Technologies Area
    - Change Global Button Color

--- js/plugins.js
+++ js/plugins.js
@@ -429,3 +429,18 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     });
 
+
+  function openCity(evt, formsName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(formsName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen").click();
